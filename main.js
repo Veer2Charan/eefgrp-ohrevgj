84166d18ab6f279e2874456f95e4f28f61557309
Veer2Charan <90391925+Veer2Charan@users.noreply.github.com>
2022-10-22 08:41:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+noseX=0;
+noseY=0;
+function preload() {
+    moustache= loadImage("https://i.postimg.cc/9Xkn78pW/noustache.png");
+    
+}
+function setup() {
+    canvas=createCanvas(300, 300)
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+function modelLoaded() {
+    console.log('PoseNet is initialized')
+}
+function gotPoses(results) {
+if(results.length > 0) {
+    console.log(results);
+    noseX=results[0].pose.nose.x-14;
+    noseY=results[0].pose.nose.y+1;
+    console.log("nose x =" + noseX);
+    console.log("nose y =" + noseY);
+}
+}
+
+function draw() {
+image(video,0, 0, 300, 300);
+image(moustache, noseX, noseY, 25, 25);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png')
+}
